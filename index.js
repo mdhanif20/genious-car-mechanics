185db7ead9bc913469444bd242231fc0fe4984ff
mdhainf20 <mdhanif115825@gmail.com>
2021-10-30 11:42:39 +0600

    port addded

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // name: myMechanics
